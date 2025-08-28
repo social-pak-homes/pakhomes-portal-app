a7eecb6c65359e5ae0effb09a3effe61d489aa49
jomerventolero <jomer.ventolero@citc-ph.com>
2025-08-28 22:28:02 +0800

    trigger deployment

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-28 bg-primary text-white">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-28 bg-primary text-white"> 
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Image src={"/logo.png"} alt="logo" className="object-cover" width={64} height={64}/>
